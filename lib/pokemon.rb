19413cc2918b1a24fc846375bc0b36ea3bf6a38f
Saige Leslie <saigeleslie@gmail.com>
2018-05-02 10:24:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(db)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+  end
+  
+  def self.save(name, type, id=nil) 
+    
+  end
+  
+  def self.find 
+    
+  end
+  
 end
